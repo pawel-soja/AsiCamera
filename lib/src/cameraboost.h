294470f82e071e3257a2aaa1d92eb259a991e780
Pawel Soja <pawel.soja@egzotech.com>
2020-12-10 16:31:31 +0100

    bigger bulk transfer, less total transfers

--- lib/src/cameraboost.h
+++ lib/src/cameraboost.h
@@ -42,9 +42,9 @@ class CameraBoost
 public:
     enum // constants
     {
-        Transfers = 3,   // TODO limit to maximum possible transfers
+        Transfers = 2,   // TODO limit to maximum possible transfers
         InitialBuffers = Transfers + 1,
-        MaximumTransferChunkSize = 1024*1024 // 
+        MaximumTransferChunkSize = 2*1024*1024 //
     };
 
 public:
