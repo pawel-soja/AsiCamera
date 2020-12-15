41d4d998b1fbccf6c095cec45f40fd0ee4ce9a3a
Pawel Soja <kernel32.pl@gmail.com>
2020-12-15 23:32:10 +0100

    use std::vector

--- lib/src/libusb-cpp/libusbchunkedbulktransfer.cpp
+++ lib/src/libusb-cpp/libusbchunkedbulktransfer.cpp
@@ -16,37 +16,34 @@
 #include "libusbchunkedbulktransfer.h"
 
 #include <libusb-1.0/libusb.h>
-#include <stdio.h>
+#include <cstdio>
 
 #include <algorithm>
+#include <vector>
 
 class LibUsbChunkedBulkTransferPrivate
 {
 public:
-    int count;
     int done;
     int pending;
     int completed;
     int actualLength;
-    libusb_transfer **transfer;
+    std::vector<libusb_transfer *> transfers;
     
     LibUsbChunkedBulkTransferPrivate(libusb_device_handle *dev, uint8_t endpoint, void *buffer, size_t length, size_t chunk, uint timeout)
-        : count((length + chunk - 1) / chunk)
-        , done(0)
+        : done(0)
         , pending(0)
         , completed(1)
         , actualLength(0)
-        , transfer(new libusb_transfer* [count])
+        , transfers((length + chunk - 1) / chunk)
     {
         unsigned char *pbuffer = reinterpret_cast<unsigned char *>(buffer);
 
-        for(int i=0; i<count; ++i)
+        for(auto &transfer: transfers)
         {
             ssize_t size = std::min(chunk, length);
-            transfer[i] = libusb_alloc_transfer(0);
-            libusb_fill_bulk_transfer(transfer[i], dev, endpoint, pbuffer, size, &callback, this, timeout);
-            transfer[i]->flags = 0;
-            transfer[i]->status = LIBUSB_TRANSFER_ERROR;
+            transfer = libusb_alloc_transfer(0);
+            libusb_fill_bulk_transfer(transfer, dev, endpoint, pbuffer, size, &callback, this, timeout);
             pbuffer += size;
             length -= size;
         }
@@ -54,10 +51,8 @@ public:
 
     virtual ~LibUsbChunkedBulkTransferPrivate()
     {
-        for(int i=0; i<count; ++i)
-            libusb_free_transfer(transfer[i]);
-
-        delete [] transfer;
+        for(auto &transfer: transfers)
+            libusb_free_transfer(transfer);
     }
 
     static void callback(libusb_transfer *transfer)
@@ -78,9 +73,12 @@ public:
     void cancel()
     {
         //fprintf(stderr, "[LibUsbChunkedBulkTransferPrivate::cancel]\n");
-        for(int i=0; i<pending; ++i)
+        auto transfer = transfers.begin();
+        auto end = transfer + pending;
+
+        for(; transfer != end; ++transfer)
         {
-            int rc = libusb_cancel_transfer(transfer[i]);
+            int rc = libusb_cancel_transfer(*transfer);
             if (rc != LIBUSB_SUCCESS)
                 continue;
             //fprintf(stderr, "must wait! %d %d\n", i, transfer[i]->status);
@@ -117,13 +115,6 @@ LibUsbChunkedBulkTransfer &LibUsbChunkedBulkTransfer::operator=(LibUsbChunkedBul
     return *this;
 }
 
-#if 0
-libusb_transfer *LibUsbChunkedBulkTransfer::handle()
-{
-    return d_func()->transfer;
-}
-#endif
-
 LibUsbChunkedBulkTransfer& LibUsbChunkedBulkTransfer::submit()
 {
     LibUsbChunkedBulkTransferPrivate *d = d_func();
@@ -131,9 +122,9 @@ LibUsbChunkedBulkTransfer& LibUsbChunkedBulkTransfer::submit()
     d->completed = 0;
     d->actualLength = 0;
     d->pending = 0;
-    for(int i=0; i<d->count; ++i)
+    for(auto &transfer: d->transfers)
     {
-        int rc = libusb_submit_transfer(d->transfer[i]);
+        int rc = libusb_submit_transfer(transfer);
         if (rc != 0)
         {
             fprintf(stderr, "[LibUsbChunkedBulkTransfer::submit]: fail %s\n", libusb_error_name(rc));
@@ -151,7 +142,6 @@ LibUsbChunkedBulkTransfer& LibUsbChunkedBulkTransfer::cancel()
     //fprintf(stderr, "[LibUsbChunkedBulkTransfer::cancel]\n");
     d_func()->cancel();
     wait();    
-    // TODO event
     return *this;
 }
 
@@ -162,12 +152,14 @@ LibUsbChunkedBulkTransfer& LibUsbChunkedBulkTransfer::wait(uint timeout)
 {
     LibUsbChunkedBulkTransferPrivate *d = d_func();
     std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
-    //libusb_context *ctx = HANDLE_CTX(mTransfer->dev_handle);
     timeval tv;
+
     tv.tv_usec = 10 * 1000;
     tv.tv_sec = 0;
+
     if (d->pending == 0)
         return *this;
+
     while(!d->completed)
     {
         int rc = libusb_handle_events_timeout_completed(NULL, &tv, &d->completed);
@@ -197,20 +189,6 @@ LibUsbChunkedBulkTransfer& LibUsbChunkedBulkTransfer::wait(uint timeout)
     }
     d->pending = 0;
     return *this;
-    if (d->done != d->count)
-    {
-        fprintf(stderr, "[LibUsbChunkedBulkTransfer::wait]: error was corrupted, %d/%d\n", d->done, d->count);
-        //cancel();
-    }
-    for(int i=0; i<d->count; ++i)
-    {
-        if (d->transfer[i]->status != LIBUSB_TRANSFER_COMPLETED)
-        {
-            fprintf(stderr, "[LibUsbChunkedBulkTransfer::wait]: %d %d ...\n", i, d->transfer[i]->status);
-            break;
-        }
-    }
-    return *this;
 }
 
 LibUsbChunkedBulkTransfer& LibUsbChunkedBulkTransfer::transfer()
@@ -228,17 +206,19 @@ uint LibUsbChunkedBulkTransfer::actualLength() const
 void *LibUsbChunkedBulkTransfer::buffer()
 {
     LibUsbChunkedBulkTransferPrivate *d = d_func();
-    return d->transfer[0]->buffer;
+    return d->transfers[0]->buffer;
 }
 
 LibUsbChunkedBulkTransfer& LibUsbChunkedBulkTransfer::setBuffer(void *buffer)
 {
     LibUsbChunkedBulkTransferPrivate *d = d_func();
+
     unsigned char *pbuffer = reinterpret_cast<unsigned char*>(buffer);
-    for(int i=0; i<d->count; ++i)
+
+    for(auto &transfer: d->transfers)
     {
-        d->transfer[i]->buffer = pbuffer;
-        pbuffer += d->transfer[i]->length;
+        transfer->buffer = pbuffer;
+        pbuffer += transfer->length;
     }
 
     return *this;
@@ -247,10 +227,10 @@ LibUsbChunkedBulkTransfer& LibUsbChunkedBulkTransfer::setBuffer(void *buffer)
 LibUsbChunkedBulkTransfer& LibUsbChunkedBulkTransfer::setDevice(libusb_device_handle *dev)
 {
     LibUsbChunkedBulkTransferPrivate *d = d_func();
-    for(int i=0; i<d->count; ++i)
-    {
-        d->transfer[i]->dev_handle = dev;
-    }
+
+    for(auto &transfer: d->transfers)
+        transfer->dev_handle = dev;
+
     return *this;
 }
 
